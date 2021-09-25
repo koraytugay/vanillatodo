58873a25553924d93797773b13869d6ba3bf6933
Koray Tugay <koraytugay@icloud.com>
2021-09-24 20:51:23 -0400

    review improvements (#1)

--- src/todoController.js
+++ src/todoController.js
@@ -1,67 +1,55 @@
-import todoService from './todoService.js';
+import * as todoService from './todoService.js';
 
-(function() {
-  const findById = id => document.querySelector(`#${id}`);
-  const createElement = (tag, options) => document.createElement(tag, options);
+const findById = id => document.querySelector(`#${id}`);
+const createElement = (name, props = {}) => Object.assign(document.createElement(name), props);
 
-  window.addEventListener('load', function() {
+window.addEventListener('load', refreshUi);
+
+findById('new-todo-input').addEventListener('keydown', e => {
+  if ('Enter' === e.key && e.target.value) {
+    new todoService.Todo(e.target.value);
+    e.target.value = '';
     refreshUi();
+  }
+});
+
+findById('delete-all-todo-items').addEventListener('click', () => {
+  todoService.deleteTodoItems();
+  refreshUi();
+});
+
+function toggleTodoCompleteStatus(id) {
+  todoService.toggleTodoItemCompletedStatus(id);
+  refreshUi();
+}
+
+function deleteTodoItem(id) {
+  todoService.deleteTodoItem(id);
+  refreshUi();
+}
+
+function createTodoDiv({text, id, completed}) {
+  const markAsDoneCheckbox = createElement('input', {type: 'checkbox', checked: completed});
+  markAsDoneCheckbox.addEventListener('click', () => toggleTodoCompleteStatus(id));
+
+  const todoSpan = createElement('span', {textContent: text});
+  if (completed) {
+    todoSpan.classList.add('done');
+  }
+
+  const deleteTodoItemLink = createElement('a', {href: '#', textContent: '❌'});
+  deleteTodoItemLink.addEventListener('click', () => deleteTodoItem(id));
+
+  const todoDiv = createElement('div', {id: id});
+  [markAsDoneCheckbox, todoSpan, deleteTodoItemLink].forEach(element => {
+    todoDiv.appendChild(element);
   });
 
-  findById('new-todo-input').addEventListener('keydown', e => {
-    if ('Enter' === e.key && e.target.value) {
-      new todoService.Todo(e.target.value);
-      e.target.value = '';
-      refreshUi();
-    }
-  });
+  return todoDiv;
+}
 
-  findById('delete-all-todo-items').addEventListener('click', e => {
-    todoService.deleteTodoItems();
-    refreshUi();
-  });
-
-  function toggleTodoCompleteStatus(id) {
-    todoService.toggleTodoItemCompletedStatus(id);
-    refreshUi();
-  }
-
-  function deleteTodoItem(id) {
-    todoService.deleteTodoItem(id);
-    refreshUi();
-  }
-
-  function createTodoDiv(todoItem) {
-    const todoDiv = createElement('div');
-    todoDiv.id = todoItem.id;
-
-    const markAsDoneCheckbox = createElement('input');
-    markAsDoneCheckbox.setAttribute('type', 'checkbox');
-    markAsDoneCheckbox.addEventListener('click', () => toggleTodoCompleteStatus(todoItem.id));
-
-    const todoSpan = createElement('span');
-    todoSpan.innerText = todoItem.text;
-
-    const deleteTodoItemLink = createElement('a');
-    deleteTodoItemLink.setAttribute('href', '#');
-    deleteTodoItemLink.addEventListener('click', () => deleteTodoItem(todoItem.id));
-    deleteTodoItemLink.innerText = '❌';
-
-    if (todoItem.completed) {
-      todoSpan.classList.add('done');
-      markAsDoneCheckbox.setAttribute('checked', 'checked');
-    }
-
-    todoDiv.appendChild(markAsDoneCheckbox);
-    todoDiv.appendChild(todoSpan);
-    todoDiv.appendChild(deleteTodoItemLink);
-
-    return todoDiv;
-  }
-
-  function refreshUi() {
-    const todoItemsDiv = findById('todo-items');
-    todoItemsDiv.innerHTML = '';
-    todoService.getTodoItems().forEach(todoItem => todoItemsDiv.appendChild(createTodoDiv(todoItem)));
-  }
-})();
+function refreshUi() {
+  const todoItemsDiv = findById('todo-items');
+  todoItemsDiv.innerHTML = '';
+  todoService.getTodoItems().forEach(todoItem => todoItemsDiv.appendChild(createTodoDiv(todoItem)));
+}
--- src/todoService.js
+++ src/todoService.js
@@ -1,28 +1,27 @@
-import todoItemsStorageService from './todoItemsStorageService.js';
+import * as todoItemsStorageService from './todoItemsStorageService.js';
 
-export default (function() {
-  function Todo(text) {
-    this.text = text;
-    this.id = `todo-item-${todoItemsStorageService.getNextTodoItemId()}`;
-    this.completed = false;
-    todoItemsStorageService.addTodoItem(this);
-  }
+function Todo(text) {
+  this.text = text;
+  this.id = `todo-item-${todoItemsStorageService.getNextTodoItemId()}`;
+  this.completed = false;
+  todoItemsStorageService.addTodoItem(this);
+}
 
-  function toggleTodoItemCompletedStatus(id) {
-    const todoItem = todoItemsStorageService.getTodoItem(id);
-    todoItem.completed = !todoItem.completed;
-    todoItemsStorageService.updateTodoItem(todoItem);
-  }
+function toggleTodoItemCompletedStatus(id) {
+  const todoItem = todoItemsStorageService.getTodoItem(id);
+  todoItem.completed = !todoItem.completed;
+  todoItemsStorageService.updateTodoItem(todoItem);
+}
 
-  const getTodoItems = () => todoItemsStorageService.getTodoItems();
-  const deleteTodoItem = (id) => todoItemsStorageService.deleteTodoItem(id);
-  const deleteTodoItems = () => todoItemsStorageService.deleteTodoItems();
+const getTodoItems = () => todoItemsStorageService.getTodoItems();
+const deleteTodoItem = (id) => todoItemsStorageService.deleteTodoItem(id);
+const deleteTodoItems = () => todoItemsStorageService.deleteTodoItems();
 
-  return {
-    Todo,
-    getTodoItems,
-    toggleTodoItemCompletedStatus,
-    deleteTodoItem,
-    deleteTodoItems
-  };
-}());
+export {
+  Todo,
+  getTodoItems,
+  toggleTodoItemCompletedStatus,
+  deleteTodoItem,
+  deleteTodoItems
+}
+
